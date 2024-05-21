c8cc73e6f59f979dc4238406fe468b52714b286a
Abner Diaz <abner.diaz@educatic.unam.mx>
2024-05-20 19:41:35 -0600

    primer comit

---
+++ tailwind.config.js
@@ -0,0 +1,46 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}",'*.{html,js}'], // Like this
+  
+  theme: {
+    extend: {
+      backgroundImage:{
+        'sanFrancisco': "url('../src/img/sanFrancisco.jpg')", 
+        'sanFranciscoDesktop': "url('../src/img/sanFranciscoDesktop.jpg')",
+        'yosemite': "url('../src/img/yosemite.jpg')",
+        'LA': "url('../src/img/LA.jpg')", 
+        'seattle': "url('../src/img/seattle.jpg')", 
+        'new_york': "url('../src/img/new_york.jpg')", 
+        'norway': "url('../src/img/norway.jpg')", 
+        'sydney': "url('../src/img/sydney.jpg')", 
+        'miami': "url('../src/img/miami.jpg')", 
+        'switzerland': "url('../src/img/switzerland.jpg')", 
+        'bali': "url('../src/img/bali.jpg')", 
+        'chicago': "url('../src/img/chicago.jpg')", 
+        'europe': "url('../src/img/europe.jpg')", 
+        'iceland': "url('../src/img/iceland.jpg')"
+      },
+      backgroundColor: theme =>({ 
+      ...theme('colors'), 
+      'primary': '#CC2D4A', 
+      'secondary': '#8FA206', 
+      'tertiary': '#61AEC9', 
+      }), 
+      textColor:{ 
+        'primary': '#CC2D4A', 
+        'secondary': '#8FA206', 
+        'tertiary': '#61AEC9', 
+      },
+      fontFamily:{
+        Montserrat:['Montserrat', 'sans-serif']
+      },
+      colors:{
+        adgycesm:{
+          50:'#BE33FF'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
